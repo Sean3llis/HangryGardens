6c0cfe8908640dd6cd01d92d83debd3109fddb1d
SEAN <seane235@gmail.com>
2015-09-16 14:59:17 -0400

    centered logo

--- templates/splash.php
+++ templates/splash.php
@@ -1,13 +1,17 @@
-<?php
-/**
- * Template Name: Splash Page
- */
-?>
+<?php /* Template Name: Splash Page */ ?>
 
 <?php wp_head(); ?>
 <?php $image = get_field('splash_bg'); ?>
-<html style="background: url('<?php echo  ?>') no-repeat center center fixed;">
+<html style="background: url('<?php echo $image['url']; ?>') no-repeat center center fixed;
+	-webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  margin-top: 0px !important">
 	<body style="background-color: transparent;">
-		
+		<div class="logo-wrapper">
+			<?php $logo = get_field('logo'); ?>
+			<img src="<?php echo $logo['url']; ?>" alt="<?php echo $logo['alt']; ?>">
+		</div>
 	</body>
 </html>
